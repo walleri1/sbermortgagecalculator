573830a574325b29a9710e2c9a989d3a068fd9ca
Vitaliy Turov <walleri1@yandex.ru>
2025-03-02 11:50:04 +0300

    feature/task-7: Add paths /execute, /cache. Add config release images Dockerfile Adding paths for a service with mock data. Adding configuration to the release image

--- cmd/mortgage_calculator/main.go
+++ cmd/mortgage_calculator/main.go
@@ -2,9 +2,34 @@
 package main
 
 import (
+	"flag"
+	"fmt"
 	"log"
+	"net/http"
+	"sbermortgagecalculator/internal/routes"
+	"sbermortgagecalculator/internal/utils"
+
+	"github.com/gorilla/mux"
 )
 
 func main() {
-	log.Println("mortgage_calculator")
+	configPath := flag.String("config", "config.yml", "The path to the configuration file")
+	flag.Parse()
+	if configPath == nil {
+		log.Fatalln("Error set config path")
+	}
+	config, err := utils.LoadConfig(*configPath)
+	if err != nil {
+		log.Fatalf("Error load config server: %v", err)
+	}
+
+	r := mux.NewRouter()
+
+	routes.SetupRoutes(r)
+
+	address := fmt.Sprintf(":%d", config.Port)
+	fmt.Printf("The server is running on the port %s\n", address)
+	if err := http.ListenAndServe(address, r); err != nil {
+		log.Fatalf("Server startup error: %v", err)
+	}
 }
--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module sbermortgagecalculator
 
 go 1.21
 
-require gopkg.in/yaml.v3 v3.0.1
+require (
+	github.com/gorilla/mux v1.8.1
+	gopkg.in/yaml.v3 v3.0.1
+)
