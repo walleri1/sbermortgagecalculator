454d28998cb981e1778b4a8298a2849daada8b1b
Vitaliy Turov <walleri1@yandex.ru>
2025-03-02 12:30:15 +0300

    feature/task-7: Append paths, append swagger (#15) (#16)
    * Hooks pre-commit add
    
    * Rename branch lint workflows
    
    * feature/task-7: Append utils
    Adding to utilities reading the application configuration using yaml configs
    
    * feature/task-7: Add paths /execute, /cache. Add config release images Dockerfile
    Adding paths for a service with mock data. Adding configuration to the release image
    
    * feature/task-7: Append run command service Makefile
    
    * feature/task-7: Adding swagger for a nice check of the service

--- internal/models/models.go
+++ internal/models/models.go
@@ -1,2 +1,49 @@
 // Package models contains data structures and database interaction logic.
 package models
+
+// LoanParams stores the user's request parameters
+type LoanParams struct {
+	ObjectCost     int `json:"object_cost"`     // Cost object
+	InitialPayment int `json:"initial_payment"` // Initial payment
+	Months         int `json:"months"`          // Loan term in months
+}
+
+// Program describes the selected loan program
+type Program struct {
+	Salary   bool `json:"salary,omitempty"`   // Corporate program
+	Military bool `json:"military,omitempty"` // Military program
+	Base     bool `json:"base,omitempty"`     // Base program
+}
+
+// Aggregates describes the results of loan calculations
+type Aggregates struct {
+	Rate            int    `json:"rate"`              // Annual interest rate
+	LoanSum         int    `json:"loan_sum"`          // Credit amount
+	MonthlyPayment  int    `json:"monthly_payment"`   // Monthly payment
+	Overpayment     int    `json:"overpayment"`       // Overpayment for the entire period
+	LastPaymentDate string `json:"last_payment_date"` // Last payment date
+}
+
+// LoanRequest is a structure representing a JSON request
+type LoanRequest struct {
+	Params  LoanParams `json:"params"`
+	Program Program    `json:"program"`
+}
+
+// CalculationResult combines a query and a calculation result
+type CalculationResult struct {
+	Params     LoanParams `json:"params"`
+	Program    Program    `json:"program"`
+	Aggregates Aggregates `json:"aggregates"`
+}
+
+// LoanResponse structure for the response
+type LoanResponse struct {
+	Result CalculationResult `json:"result"`
+}
+
+// CachedLoan is a structure for storing data in a cache
+type CachedLoan struct {
+	ID     int               `json:"id"`
+	Result CalculationResult `json:"result"`
+}
--- internal/routes/routes.go
+++ internal/routes/routes.go
@@ -1,2 +1,14 @@
-// Package routes ...
+// The routes package implements all service paths
 package routes
+
+import (
+	"github.com/gorilla/mux"
+
+	"sbermortgagecalculator/internal/routes/paths"
+)
+
+// SetupRoutes sets handlers for paths
+func SetupRoutes(router *mux.Router) {
+	router.HandleFunc("/execute", paths.ExecuteLoanCalculation).Methods("POST")
+	router.HandleFunc("/cache", paths.GetCachedLoans).Methods("GET")
+}
