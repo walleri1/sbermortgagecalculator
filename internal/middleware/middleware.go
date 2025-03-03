eb6ac73b34fec64267fa09fec0f0ad66a84faa3e
Vitaliy Turov <walleri1@yandex.ru>
2025-03-03 08:12:54 +0300

    feature/task-11: Append middelware logs paths

--- internal/middleware/middleware.go
+++ internal/middleware/middleware.go
@@ -1,2 +1,30 @@
 // Package middleware ...
 package middleware
+
+import (
+	"log"
+	"net/http"
+	"time"
+)
+
+func LoggingMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		begin := time.Now()
+		rw := &responseWriter{w, http.StatusOK}
+
+		next.ServeHTTP(rw, r)
+
+		duration := time.Since(begin)
+		log.Printf("status_code: %d, duration: %s ns\n", rw.statusCode, duration)
+	})
+}
+
+type responseWriter struct {
+	http.ResponseWriter
+	statusCode int
+}
+
+func (rw *responseWriter) WriteHeader(code int) {
+	rw.statusCode = code
+	rw.ResponseWriter.WriteHeader(code)
+}
